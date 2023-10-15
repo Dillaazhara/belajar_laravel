8c93a9885ce2fd120ef3169ead09a1de05ad40d6
Dilla Azhara <azharadilla05@gmail.com>
2023-10-15 18:21:04 +0700

    Add Method datadiri

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,5 +9,9 @@ class HomeController extends Controller
     public function home(){
          return view('welcome');
      }
+
+     public function datadiri(){
+    return ('Nama : Dilla Azhara Hayati <br> NIM : D212011057');
+    }
 }
 
